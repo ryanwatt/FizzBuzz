876c2adc7cf9acb1d8b0c299e094e07ec88143a8
Ryan Watt <ryanwatt24@gmail.com>
2015-02-03 15:54:32 -0700

    FinishedFizzBuzz

--- FizzBuzz-Objc/FizzBuzz-Objc/AppDelegate.m
+++ FizzBuzz-Objc/FizzBuzz-Objc/AppDelegate.m
@@ -15,7 +15,45 @@
     
     // Insert code here
     
+    for (int i = 1; i < 101; i++){
+        BOOL buzzed = [self isBuzzed:i];
+        BOOL fizzed = [self isFizzed:i];
+        
+        if (buzzed && !fizzed){
+            NSLog(@"Buzz");
+        }else if (fizzed && !buzzed){
+            NSLog(@"Fizz");
+        }else if (fizzed && buzzed){
+            NSLog(@"FizzBuzz");
+        }else{
+            NSLog(@"%d", i);
+        }
+        
+    }
+    
+    
+    
     return YES;
 }
+-(BOOL)isBuzzed:(int)integer {
+    if (integer % 3 == 0){
+        return true;
+    }else if ([[NSString stringWithFormat: @"%d", integer] containsString:@"3"]) {
+        return false;
+    }else{
+        return false;
+    }
+    
+}
+-(BOOL)isFizzed:(int)integer {
+    if (integer % 5 == 0){
+        return true;
+    }else if ([[NSString stringWithFormat: @"%d", integer] containsString:@"5"]) {
+        return false;
+    }else{
+        return false;
+    }
+}
+
 
 @end
